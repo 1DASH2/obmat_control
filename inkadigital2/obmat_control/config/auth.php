a803872c3ff65b305b5a23c77a7645a868ec983d
Gianfranco Ore <gian.oj2090@gmail.com>
2026-06-02 11:59:25 -0500

    Elimina inkadigital2 (contenía otro obmat_control igual)

--- inkadigital2/obmat_control/config/auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['usuario'])) {
-    header("Location: ../index.php");
-    exit();
-}
-// Puedes añadir lógica adicional de tiempo de inactividad aquí
-?>
